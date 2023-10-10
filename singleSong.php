6c0f8e93694eae3b01711d3d3d68e3eae5eba163
ardausuk <ausuk388@mtroyal.ca>
2023-10-09 23:24:32 -0600

    html and css header, nav, and footer added

--- singleSong.php
+++ singleSong.php
@@ -4,11 +4,32 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+  
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/single.css">
 </head>
 <body>
-    
+    <header>
+
+
+<div class="navbar">
+    <p class="title">HarmonyHub</p>
+    <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">Search</a></li>
+        <li><a href="#">Favourites</a></li>
+        <li><a href="#">Browse</a></li>
+    </ul>
+</div>
+</header>
+
+<footer>
+<p>COMP 3512 Assignment 1</p>
+        <p>&copy; 2023 Arda Usuk. All Rights Reserved.</p>
+        <p>GitHub Repository: <a href="https://github.com/ardausuk/Assignment1-comp3512">HarmonyHub</a></p>
+     
+</footer>
 </body>
 </html>
