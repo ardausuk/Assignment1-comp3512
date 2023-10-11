6c878d000c016a5dac74209aed7af964197114a7
ardausuk <ausuk388@mtroyal.ca>
2023-10-10 20:35:52 -0600

    about us page created and fixes to nav bar

--- aboutPage.php
+++ aboutPage.php
@@ -8,7 +8,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>HarmonyHub</title>
-    <link rel="stylesheet" href="css/single.css">
+    <link rel="stylesheet" href="css/about.css">
 </head>
 <body>
     <header>
@@ -20,18 +20,19 @@
     <li><a href="homePage.php">Home</a></li>
         <li><a href="searchPage.php">Search</a></li>
         <li><a href="favPage.php">Favourites</a></li>
-        <li><a href="#">Browse</a></li>
+        <li><a href="resultsPage.php">Browse</a></li>
         <li><a href="aboutPage.php">About Us</a></li>
 
     </ul>
 </div>
 </header>
-
-
-
 <main>
-
+<h1>About Us</h1>
+<p>This page was created for COMP 3512 Assignment. It consists of five PHP pages, the home page contains a variety of specialized lists that are limited to 10 items using different SQL queries. Our search page allows the user to filter songs by a variety of search criteria which will then take the user to the Browse page. The Browse page displays the song title, artist name, year, genre name and popularity score. They can enter the Browse page from the Search page or through the navigation bar. The Single Song page contains information when requested by the user, users can navigate to this page through the home page, the search results page, or the view favourites page. Finally, our Favourites page will display all the songs currently in the favourites songs list, users will be able to clear to favourites via the remove all button and also be able to remove an individual song from the list, this page has the same format as our Browse page. </p> 
+<img src="/img/about.jpeg" alt="record-table" >
+</main>
 <footer>
+
 <p>COMP 3512 Assignment 1</p>
         <p>&copy; 2023 Arda Usuk. All Rights Reserved.</p>
         <p>GitHub Repository: <a href="https://github.com/ardausuk/Assignment1-comp3512">HarmonyHub</a></p>
