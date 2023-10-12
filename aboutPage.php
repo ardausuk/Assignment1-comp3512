c15e96a1d8a56e57d78c656e25695f6d2cf655f9
ardausuk <ausuk388@mtroyal.ca>
2023-10-11 21:31:19 -0600

    about us formatting

--- aboutPage.php
+++ aboutPage.php
@@ -29,7 +29,8 @@
 <main>
 <h1>About Us</h1>
 <p>This page was created for COMP 3512 Assignment. It consists of five PHP pages, the home page contains a variety of specialized lists that are limited to 10 items using different SQL queries. Our search page allows the user to filter songs by a variety of search criteria which will then take the user to the Browse page. The Browse page displays the song title, artist name, year, genre name and popularity score. They can enter the Browse page from the Search page or through the navigation bar. The Single Song page contains information when requested by the user, users can navigate to this page through the home page, the search results page, or the view favourites page. Finally, our Favourites page will display all the songs currently in the favourites songs list, users will be able to clear to favourites via the remove all button and also be able to remove an individual song from the list, this page has the same format as our Browse page. </p> 
-<img src="/img/about.jpeg" alt="record-table" >
+
+
 </main>
 <footer>
 
