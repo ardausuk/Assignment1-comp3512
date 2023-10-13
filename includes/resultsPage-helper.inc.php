2bb82f286a8b7204951d5dd814c7ef7d7955ddfe
ardausuk <ausuk388@mtroyal.ca>
2023-10-13 14:33:09 -0600

    results page complete

---
+++ includes/resultsPage-helper.inc.php
@@ -0,0 +1,50 @@
+<?php
+
+# Ensures that the song title max length is 25 followed by hellip
+function truncateTitle($title, $max_length = 25){
+    if (strlen($title) > $max_length) {
+        $title = htmlspecialchars(substr($title, 0, $max_length - 1)) . '&hellip;';
+        }
+
+    return $title;
+    }
+
+    # Function the outputs the browse table and later for results of the search 
+    function outputSearchResults($songs, $name, $search){
+        echo "<table>";
+        echo "<tr>";
+        echo "<th>Title</th>";
+        echo "<th>Artist</th>";
+        echo "<th>Year</th>";
+        echo "<th>Genre</th>";
+        echo "<th>Popularity</th>";
+        echo "<th>Add to Favourites</th>";
+        echo "<th>View Song</th>";
+        echo "</tr>";
+        foreach($songs as $s){ ?>
+            <tr>
+            <td class='song-title'><a href='singleSong.php?id=<?= $s['song_id'] ?>'><?= truncateTitle($s['title']) ?></a></td>
+                <td><?=$s['artist_name']?></td>
+                <td><?=$s['year']?></td>
+                <td><?=$s['genre_name']?></td>
+                <td><?=$s['popularity']?></td>
+                <td><a href='favPage.php?id=<?=$s['song_id']?>&name=<?=$name?>&<?=$name?>=<?=$search?>' ><button class='button'>Add</button></a></td>
+                <td><a href='singleSong.php?id=<?=$s['song_id']?>' ><button class='view'>View</button></a></td>
+            </tr>
+        <?php }
+        echo "</table>";
+    } 
+    # Function that outputs the genre from the search into the browse table
+    function outputGenre($songs){
+        foreach($songs as $key){
+            echo "<option value='".$key['genre_id']."'>".$key['genre_name']."</option>";
+        }
+
+    }
+    # Function that outputs the artist from search into the browse table
+    function outputArtistList($artist){
+        foreach($artist as $key){
+            echo "<option value='".$key['artist_id']."'>".$key['artist_name']."</option>";
+        }
+    }
+?>
--- resultsPage.php
+++ resultsPage.php
@@ -1,4 +1,62 @@
 <?php
+ require_once 'includes/config.inc.php';
+ require_once 'includes/db-classes.inc.php';
+ require_once 'includes/resultsPage-helper.inc.php';
+
+ try{
+     $conn = DatabaseHelper::createConnection(array(DBCONNSTRING, DBUSER, DBPASS));
+     $songsGet = new SongsDB($conn);
+     $artistGet = new ArtistsDB($conn);
+     $genreGet = new GenresDB($conn);
+
+     $name="";
+
+     if( !empty($_GET['title']) ){
+         $songs = $songsGet->getAllWithTitle($_GET['title']);
+         $message = "Showing all songs with '" . $_GET['title'] . "' in Title";
+         $name = "title";
+     }
+     else if( !empty($_GET['artistList']) && $_GET['artistList'] > 0){
+         $artist_data = $artistGet->getArtist($_GET['artistList']);
+         $songs = $songsGet->getAllForArtist($artist_data[0]['artist_name']);
+         $message = "Showing all songs by " . $artist_data[0]['artist_name'];
+         $name = "artistList";
+     }
+     else if( !empty($_GET['genreList']) && $_GET['genreList'] > 0 ){
+         $genre_data = $genreGet->getGenre($_GET['genreList']);
+         $songs = $songsGet->getAllForGenre($genre_data[0]['genre_name']);
+         $message = "Showing all " . $genre_data[0]['genre_name'] . " songs in Genre";
+         $name = "genreList";
+     }
+     else if( !empty($_GET['year-before-value']) ){
+         $songs = $songsGet->getAllBeforeYear($_GET['year-before-value']);
+         $message = "Showing all songs before the year " . $_GET['year-before-value'];
+         $name = "year-before-value";
+     }
+     else if( !empty($_GET['year-after-value']) ){
+         $songs = $songsGet->getAllAfterYear($_GET['year-after-value']);
+         $message = "Showing all songs after the year " . $_GET['year-after-value'];
+         $name = "year-after-value";
+     }
+     else if( !empty($_GET['pop-less-value']) ){
+         $songs = $songsGet->getAllPopularityLess($_GET['pop-less-value']);
+         $message = "Showing all songs with popularity less than " . $_GET['pop-less-value'];
+         $name = "pop-less-value";
+     }
+     else if( !empty($_GET['pop-greater-value']) ){
+         $songs = $songsGet->getAllPopularityGreat($_GET['pop-greater-value']);
+         $message = "Showing all songs with popularity greater than " . $_GET['pop-greater-value'];
+         $name = "pop-greater-value";
+     }
+     else{
+         $songs = $songsGet->showAllSongs();
+         $message = "Showing all songs";
+     }
+     // get query strings
+     $search = $_GET[$name];
+ } catch(Exception $e){
+     die($e->getMessage());
+ }
 ?>
 
 <!DOCTYPE html>
@@ -28,6 +86,11 @@
 <body>
     <h1>Browse For Your Songs!</h1>
 <main>
+<a href='resultsPage.php' class='show-all-button'><button>Show All</button></a>
+<?php
+echo $message;
+    outputSearchResults($songs, $name, $search); 
+?>
 
 </main>
 <footer>
